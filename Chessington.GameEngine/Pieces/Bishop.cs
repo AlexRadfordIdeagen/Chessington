6e252dd8a7c0e2aa410d5eb23eddb07136deb737
IDEAGENPLC\Alex.Radford <alex.radford@ideagen.com>
2018-04-11 15:14:09 +0100

    implemented legal moves for bishop

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -8,9 +8,38 @@ namespace Chessington.GameEngine.Pieces
         public Bishop(Player player)
             : base(player) { }
 
-        public override IEnumerable<Square> GetAvailableMoves(Board board)
+        public override IEnumerable<Square> GetAvailableMoves(Board board) 
         {
-            return Enumerable.Empty<Square>();
+
+            var myLocation = board.FindPiece(this);
+            var legalMoves = new List<Square>();
+
+            for (var i = 0; i < 8; i++)
+            {
+                var placeICanMove = new Square(myLocation.Row + i, myLocation.Col + i);
+                if (placeICanMove != myLocation & placeICanMove.Row < 8  & placeICanMove.Col < 8)
+                {
+                    legalMoves.Add(placeICanMove);
+                }
+                placeICanMove = new Square(myLocation.Row + i, myLocation.Col - i);
+                if (placeICanMove != myLocation & placeICanMove.Row < 8 & placeICanMove.Col >= 0)
+                {
+                    legalMoves.Add(placeICanMove);
+                }
+                placeICanMove = new Square(myLocation.Row - i, myLocation.Col + i);
+                if (placeICanMove != myLocation & placeICanMove.Row >= 0 & placeICanMove.Col < 8)
+                {
+                    legalMoves.Add(placeICanMove);
+                }
+                 placeICanMove = new Square(myLocation.Row - i, myLocation.Col - i);
+                if (placeICanMove != myLocation & placeICanMove.Row >= 0 & placeICanMove.Col >= 0)
+                {
+                    legalMoves.Add(placeICanMove);
+                }
+            }
+
+
+            return legalMoves;
         }
-    }
+    } 
 }
