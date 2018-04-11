3af5c22717b66de635011ed685c589f60e281727
IDEAGENPLC\Alex.Radford <alex.radford@ideagen.com>
2018-04-11 12:25:08 +0100

    Pawns can now move up and down dependant on colour

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
+        public Pawn(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var myLocation = board.FindPiece(this);
+            var legalMoves = new List<Square>();
+            CheckLegalMoves(myLocation, legalMoves);
+
+            return legalMoves;
+
+        }
+
+        private void CheckLegalMoves(Square myLocation, List<Square> legalMoves)
+        {
+            if (Player == Player.Black)
+                legalMoves.Add(new Square(myLocation.Row + 1, myLocation.Col));
+            else if (Player == Player.White)
+            {
+                legalMoves.Add(new Square(myLocation.Row - 1, myLocation.Col));
+            }
         }
     }
 }
