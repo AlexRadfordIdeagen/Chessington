6514bdc399edffdf091ffa275bb4c8cafa766a99
IDEAGENPLC\Alex.Radford <alex.radford@ideagen.com>
2018-04-11 15:36:00 +0100

    implemented legal moves for king

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,40 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var myLocation = board.FindPiece(this);
+            var legalMoves = new List<Square>();
+
+            legalMoves.Add(new Square(myLocation.Row - 1, myLocation.Col + 1));
+            legalMoves.Add(new Square(myLocation.Row - 1, myLocation.Col - 1));
+            legalMoves.Add(new Square(myLocation.Row + 1, myLocation.Col + 1));
+            legalMoves.Add(new Square(myLocation.Row + 1, myLocation.Col - 1));
+            legalMoves.Add(new Square(myLocation.Row, myLocation.Col - 1));
+            legalMoves.Add(new Square(myLocation.Row, myLocation.Col + 1));
+            legalMoves.Add(new Square(myLocation.Row + 1, myLocation.Col));
+            legalMoves.Add(new Square(myLocation.Row - 1, myLocation.Col));
+
+
+            foreach (var item in legalMoves)
+            {
+                if (item.Col < 0)
+                {
+                    legalMoves.Remove(item);
+                }
+                if (item.Col > 7)
+                {
+                    legalMoves.Remove(item);
+                }
+                if (item.Row < 0)
+                {
+                    legalMoves.Remove(item);
+                }
+                if (item.Row > 7)
+                {
+                    legalMoves.Remove(item);
+                }
+            }
+
+            return legalMoves;
         }
     }
 }
