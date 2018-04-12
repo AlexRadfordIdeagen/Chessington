0aa92beb53a7f862b12347d484e2d7aa1a25c7e3
IDEAGENPLC\Alex.Radford <alex.radford@ideagen.com>
2018-04-12 14:44:11 +0100

    implemented taking of pieces with pawn at a diagonal

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -22,45 +22,60 @@ namespace Chessington.GameEngine.Pieces
             var myLocation = board.FindPiece(this);
             var legalMoves = new List<Square>();
 
-            if (Player == Player.Black) PawnMoveCheck(board, myLocation, legalMoves, +1);
+            if (Player == Player.Black) PawnMoveCheck(board, myLocation, legalMoves, 1);
             if (Player == Player.White) PawnMoveCheck(board, myLocation, legalMoves, -1);
 
-            if (Player == Player.White && !hasMoved)
-            {
-                if (DoubleMovement(board, myLocation, legalMoves, -2))
-                {
-                    PawnMoveCheck(board, myLocation, legalMoves, -2);
-                }
-            }
-
-            if (Player == Player.Black && !hasMoved)
-            {
-                if (DoubleMovement(board, myLocation, legalMoves, +2))
-                {
-                    PawnMoveCheck(board, myLocation, legalMoves, +2);
-                }
-            }
-
+            if (Player == Player.White && !hasMoved) IsSomethingInFront(board, myLocation, legalMoves, -2);
+            
+            if (Player == Player.Black && !hasMoved) IsSomethingInFront(board, myLocation, legalMoves, 2);
+          
             return legalMoves;
         }
 
-        private static bool DoubleMovement(Board board, Square myLocation, List<Square> legalMoves, int movement)
+        private static void IsSomethingInFront(Board board, Square myLocation, List<Square> legalMoves, int movement)
         {
-            var placeICanMove = new Square(myLocation.Row + movement / 2, myLocation.Col);
-            return
-                placeICanMove.IsSquareValid() &&
-                board.GetPiece(placeICanMove) == null
-                    ;
+            var place = new Square(myLocation.Row + movement, myLocation.Col);
+            var placeInFront = new Square(myLocation.Row + movement/2, myLocation.Col);
+            if (place.IsSquareValid() && board.GetPiece(place) == null)
+            {
+                if (placeInFront.IsSquareValid() &&
+                    board.GetPiece(placeInFront) == null)
+                {
+                    legalMoves.Add(place);
+
+                }
+            }
         }
 
-        private static void PawnMoveCheck(Board board, Square myLocation, List<Square> legalMoves, int movement)
+        private void PawnMoveCheck(Board board, Square myLocation, List<Square> legalMoves, int movement)
         {
-            var placeICanMove = new Square(myLocation.Row + movement, myLocation.Col);
-            if (placeICanMove.IsSquareValid() &&
-            board.GetPiece(placeICanMove) == null
+            var place = new Square(myLocation.Row + movement, myLocation.Col);
+            var enemyCheckLeft = new Square(myLocation.Row + movement, myLocation.Col - 1);
+            var enemyCheckRight = new Square(myLocation.Row + movement, myLocation.Col + 1);
+            
+            if (place.IsSquareValid() &&
+            board.GetPiece(place) == null
                 )
             {
-                legalMoves.Add(placeICanMove);
+                legalMoves.Add(place);
+            }
+
+            if (
+                enemyCheckLeft.IsSquareValid() &&
+                board.GetPiece(enemyCheckLeft) != null &&
+                board.GetPiece(enemyCheckLeft).Player != Player
+                )
+            {
+                legalMoves.Add(enemyCheckLeft);
+            }
+
+            if (
+                enemyCheckRight.IsSquareValid() &&
+                board.GetPiece(enemyCheckRight) != null &&
+                board.GetPiece(enemyCheckRight).Player != Player
+                )
+            { 
+                legalMoves.Add(enemyCheckRight);
             }
         }
     }
